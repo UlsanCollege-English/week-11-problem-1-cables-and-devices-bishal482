f85b837d22a70c00a8805e04085a1c1e4fc5cc56
bishal482 <galanbishal20@gmail.com>
2025-11-19 09:08:05 +0900

    hjwihsjd

--- hw01/main.py
+++ hw01/main.py
@@ -1,25 +1,25 @@
-def build_graph(edges, directed=False):
-    graph = {}
+# def build_graph(edges, directed=False):
+#     graph = {}
 
-    for u, v in edges:
-        # ensure nodes exist
-        if u not in graph:
-            graph[u] = []
-        if v not in graph:
-            graph[v] = []
+#     for u, v in edges:
+#         # ensure nodes exist
+#         if u not in graph:
+#             graph[u] = []
+#         if v not in graph:
+#             graph[v] = []
 
-        # add edge u -> v
-        graph[u].append(v)
+#         # add edge u -> v
+#         graph[u].append(v)
 
-        # if undirected, also add the reverse direction
-        if not directed:
-            graph[v].append(u)
+#         # if undirected, also add the reverse direction
+#         if not directed:
+#             graph[v].append(u)
 
-    return graph
+#     return graph
 
 
-def degree_dict(graph):
-    d = {}
-    for node in graph:
-        d[node] = len(graph[node])
-    return d
+# def degree_dict(graph):
+#     d = {}
+#     for node in graph:
+#         d[node] = len(graph[node])
+#     return d
